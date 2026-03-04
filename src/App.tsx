b6b25bcf10f65082c9007f43d0a73bd0f5188e0b
boukhrisshayat3-prog <boukhrisshayat3@alumnos.cei.es>
2026-03-04 21:22:31 +0100

    nueva

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import './App.css'
+import InfoBasica from './components/Props/Props';
 
 
 
 function App() {
   return (
     <>
-  
-
+   <h1 className='bg-blue-500 text-white p-4'>Perfil Profesional</h1>
+ <InfoBasica nombre="Bouchra Boukhriss" especialidad="Desarrolladora Frontend🤪" añosExperiencia={5} />
     </>
   )
 }
